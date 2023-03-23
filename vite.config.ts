fe3206d0e386b7f789195bc4f05c6e68293df0ad
Ivan Sheychuk <ivan.sheychuk@osf-commerce.com>
2023-03-24 00:22:22 +0200

    Changed base config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: 'https://sheychuk.github.io/sheepfish/',
   plugins: [react()],
   resolve: {
     alias: {
